0b11c5769a681261c7380e69b3013684bda0bafa
Florian Meynet <59055417+Flaw74@users.noreply.github.com>
2020-01-08 13:18:48 +0100

    Bordel

--- Projet1/Joueur_competition.cs
+++ Projet1/Joueur_competition.cs
@@ -13,41 +13,19 @@ namespace Projet1
         private int point_joueur;
         private int nb__match_jouer;
         private int nb__match_gagner;
-<<<<<<< HEAD
-=======
+
         private List<DateTime> date_compet;
->>>>>>> b97700755babe40cf6399b9b7f372c25bffb20cd
 
 
         public Joueur_competition(string nom, string prenom, DateTime naissance, string adresse, long telephone, bool sexe, string ville, bool paiement) : base(nom, prenom, naissance, adresse, telephone, sexe, ville, paiement)
         {
-<<<<<<< HEAD
             this.classement=0;
             this.age=0;
             this.point_joueur=0;
             this.nb__match_gagner=0;
             this.nb__match_jouer=0;
         }
-<<<<<<< HEAD
-        public Joueur_competition(string nom,string prenom,DateTime naissance,string adresse,long telephone,bool sexe,string ville,bool paiement,int c,int a,int p , int nb_j , int nb_g)
-        : base(nom,prenom,naissance,adresse,telephone,sexe,ville,paiement)
-=======
-        public Joueur_competition(string nom,string prenom,DateTime naissance,string adresse,long telephone,bool sexe,string ville,bool paiement,int c,int a,int p , int nb_j , int nb_g) : base(nom,prenom,naissance,adresse,telephone,sexe,ville,paiement)
->>>>>>> 894ca26c23d2c74a3514be7c05e514840fd285a7
-        {
-            this.classement=c;
-            this.age=a;
-            this.point_joueur=p;
-            this.nb__match_gagner=nb_g;
-            this.nb__match_jouer=nb_j;
-=======
-            this.classement = 0;
-            this.age = 0;
-            this.point_joueur = 0;
-            this.nb__match_gagner = 0;
-            this.nb__match_jouer = 0;
 
-        }
         public Joueur_competition(string nom, string prenom, DateTime naissance, string adresse, long telephone, bool sexe, string ville, bool paiement, int c, int a, int p, int nb_j, int nb_g) : base(nom, prenom, naissance, adresse, telephone, sexe, ville, paiement)
         {
             this.classement = c;
@@ -55,8 +33,6 @@ namespace Projet1
             this.point_joueur = p;
             this.nb__match_gagner = nb_g;
             this.nb__match_jouer = nb_j;
->>>>>>> b97700755babe40cf6399b9b7f372c25bffb20cd
-
         }
 
         public double Classement
@@ -64,27 +40,20 @@ namespace Projet1
             get { return (this.classement); }
             set { this.classement = value; }
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> b97700755babe40cf6399b9b7f372c25bffb20cd
         public new int Age
         {
             get { return (this.age); }
             set { this.age = value; }
         }
-<<<<<<< HEAD
->>>>>>> 894ca26c23d2c74a3514be7c05e514840fd285a7
-=======
+
 
         public List<DateTime> Date_compet
         {
             get { return this.date_compet; }
             set { this.date_compet = value; }
         }
->>>>>>> b97700755babe40cf6399b9b7f372c25bffb20cd
+
         public int Point_joueur
         {
             get { return (this.point_joueur); }
@@ -102,8 +71,5 @@ namespace Projet1
         }
 
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b97700755babe40cf6399b9b7f372c25bffb20cd
+
