e2c56e424610494a7e8eba4d530fea887857f4b5
Florian Meynet <59055417+Flaw74@users.noreply.github.com>
2020-01-16 15:23:55 +0100

    trouver adversaire

--- Projet1/TrouverPartenaire.xaml.cs
+++ Projet1/TrouverPartenaire.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Projet1
 {
@@ -61,6 +62,41 @@ namespace Projet1
             }
             return (liste_j_c);
         }
+        private List<Joueur_loisir> Liste_joueur_loisir()
+        {
+            String[] mots;
+            string fichierMembre_loisir = "joueur_loisir.txt";
+            List<Joueur_loisir> liste_j_l = new List<Joueur_loisir>();
+            string[] lignes = File.ReadAllLines(fichierMembre_loisir);
+            for (int i = 0; i < lignes.Length; i++)
+            {
+                string ligne_num = lignes[i];
+                mots = ligne_num.Split(',');
+                Joueur_loisir j_compet = new Joueur_loisir();
+                j_compet.Nom = mots[0];
+                j_compet.Prenom = mots[1];
+                String[] date = mots[2].Split('/');
+                int d_j = int.Parse(date[0]);
+                int d_m = int.Parse(date[1]);
+                int d_a = int.Parse(date[2]);
+
+                DateTime date_n = new DateTime(d_a, d_m, d_j);
+                j_compet.Naissance = (date_n);
+                j_compet.Adresse = mots[3];
+                j_compet.Telephone = long.Parse(mots[4]);
+                if (mots[5] == "F")
+                {
+                    j_compet.Sexe = true;
+                }
+                else
+                {
+                    j_compet.Sexe = false;
+                }
+                j_compet.Ville = mots[6];
+                liste_j_l.Add(j_compet);
+            }
+            return (liste_j_l);
+        }
 
         private void Precedent(object sender, RoutedEventArgs e)
         {
@@ -71,10 +107,32 @@ namespace Projet1
 
         private void Chercher(object sender, RoutedEventArgs e)
         {
-            //int ag= int.Parse(.Text);
-            double clas = double.Parse(classement.Text);
-            Joueur_competition player = new Joueur_competition("", "", new DateTime(), "", 0, true, "", false,clas,ag,0,0,0);
-            //Liste_joueur_compet().Sort((y) => Membre.CompareP(player, y));
+            int d_j = int.Parse(jour.Text);
+            int d_m = int.Parse(mois.Text);
+            int d_a = int.Parse(annee.Text);
+            string nomm = nom.Text;
+            string pren = prenom.Text;
+            DateTime date_n = new DateTime(d_a, d_m, d_j);
+            List<Joueur_loisir> list_j_l = Liste_joueur_loisir();
+            List<Joueur_competition> list_j_c = Liste_joueur_compet();
+            string affichage = "Joueur competition du meme age : ";
+            foreach (Joueur_competition j_c in list_j_c)
+            {
+                if ((j_c.Naissance.Year == date_n.Year) && (j_c.Nom!=nomm )&&(j_c.Prenom!=pren))
+                {
+                    affichage += "\n"+ j_c.Nom + "  " + j_c.Prenom + "   " + j_c.Telephone;
+                }
+            }
+
+            affichage += "\n\n\nJoueur loisir du meme age : ";
+            foreach (Joueur_loisir j_l in list_j_l)
+            {
+                if ((j_l.Naissance.Year == date_n.Year) && (j_l.Nom != nomm) && (j_l.Prenom != pren))
+                {
+                    affichage +="\n"+ j_l.Nom+"  "+j_l.Prenom+"   "+j_l.Telephone ;
+                }
+            }
+            trouver.Text = affichage;
 
         }
     }
